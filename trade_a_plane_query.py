9e43dd35f826065fb4bd2ad98af3c86f9a0282e8
Mark Agate <magate@email.arizona.edu>
2020-08-23 13:47:56 -0700

    Alphebatize parser and constants. Lint.

--- trade_a_plane_query.py
+++ trade_a_plane_query.py
@@ -9,63 +9,130 @@ import sys
 import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-ts", "--total_to_spend", default=25000, help="Enter the total amount you are willing to spend per year in dollars without symbols or commas.", type=int)
-parser.add_argument("-mh", "--min_hours", default=125, help="Enter the minimum hours you want to fly per year as an integer.", type=int)
-parser.add_argument("-lp", "--loan_percent", default=0.07, help="Enter the loan percentage as a decimal.", type=float)
-parser.add_argument("-ll", "--loan_length", default=20, help="Enter the length of the loan in years.", type=int)
-parser.add_argument("-eoc", "--engine_overhaul_cost", default=30000, help="Enter the cost of an engine overhaul as an integer, no symbols or commas.", type=int)
-parser.add_argument("-etbo", "--engine_time_between_overhauls", default=1700, help="Enter engine time between overhauls as an integer, no symbols or commas.", type=int)
-parser.add_argument("-poc", "--prop_overhaul_cost", default=8000, help="Enter the cost of a prop overhaul as an integer, no symbols or commas.", type=int)
-parser.add_argument("-ptbo", "--prop_time_between_overhauls", default=2000, help="Enter prop time between overhauls as an integer, no symbols or commas.", type=int)
-parser.add_argument("-occ", "--oil_change_cost", default=125, help="Enter the oil change cost per 50 hours as an integer, no symbols or commas.", type=int)
-parser.add_argument("-gc", "--gas_cost", default=5.00, help="Enter the cost of gas as a float, no symbols or commas.", type=float)
-parser.add_argument("-gph", "--gallons_per_hour", default=15.0, help="Enter the average gallons per hour as a float.", type=float)
-parser.add_argument("-ins", "--insurance", default=2000, help="Enter the yearly insurance cost as an integer, no symbols or commas.", type=int)
-parser.add_argument("-maint", "--maintenance", default=3000, help="Enter the yearly maintenance cost as an integer, no symbols or commas.", type=int)
-parser.add_argument("-han", "--hangar", default=230, help="Enter the monthly hangar cost as an integer, no symbols are commas.", type=int)
-parser.add_argument("-och", "--oil_cost_per_hour", default=2.0, help="Enter the oil cost per hour as a float, no symbols or commas.", type=float)
-parser.add_argument("-o", "--output_file", default="out.csv", help="Specify outoutfile.", type=str)
-parser.add_argument("-m", "--make", default='BEECHCRAFT', help="Specify aircraft make as all capital string.", type=str)
-parser.add_argument("-mg", "--model_group", default="+35+BONANZA+SERIES", help="Specify aircraft model group, for example, '+35+BONANZA+SERIES'.", type=str)
-parser.add_argument("-mt", "--model_type", default="V35", help="Specify a model subtype as all capital string, can leave of specific, for example V35 or V35A.", type=str) 
-parser.add_argument("-dpp", "--down_payment_percent", default=0.15, help="Enter downpayment percentage as a decimal.", type=float)
+parser.add_argument( "-dpp"
+                   , "--down_payment_percent"
+                   , default=0.15
+                   , help="Enter downpayment percentage as a decimal."
+                   , type=float)
+parser.add_argument( "-eoc"
+                   , "--engine_overhaul_cost"
+                   , default=30000
+                   , help="Enter the cost of an engine overhaul as an integer, no symbols or commas."
+                   , type=int)
+parser.add_argument( "-etbo"
+                   , "--engine_time_between_overhauls"
+                   , default=1700
+                   , help="Enter engine time between overhauls as an integer, no symbols or commas."
+                   , type=int)
+parser.add_argument( "-gc"
+                   , "--gas_cost"
+                   , default=5.00
+                   , help="Enter the cost of gas as a float, no symbols or commas."
+                   , type=float)
+parser.add_argument( "-gph"
+                   , "--gallons_per_hour"
+                   , default=15.0
+                   , help="Enter the average gallons per hour as a float."
+                   , type=float)
+parser.add_argument( "-han"
+                   , "--hangar"
+                   , default=230
+                   , help="Enter the monthly hangar cost as an integer, no symbols are commas."
+                   , type=int)
+parser.add_argument( "-ins"
+                   , "--insurance"
+                   , default=2000
+                   , help="Enter the yearly insurance cost as an integer, no symbols or commas."
+                   , type=int)
+parser.add_argument( "-ll"
+                   , "--loan_length"
+                   , default=20
+                   , help="Enter the length of the loan in years."
+                   , type=int)
+parser.add_argument( "-lp"
+                   , "--loan_percent"
+                   , default=0.07
+                   , help="Enter the loan percentage as a decimal."
+                   , type=float)
+parser.add_argument( "-m"
+                   , "--make"
+                   , default='BEECHCRAFT'
+                   , help="Specify aircraft make as all capital string."
+                   , type=str)
+parser.add_argument( "-maint"
+                   , "--maintenance"
+                   , default=3000
+                   , help="Enter the yearly maintenance cost as an integer, no symbols or commas."
+                   , type=int)
+parser.add_argument( "-mg"
+                   , "--model_group"
+                   , default="+35+BONANZA+SERIES"
+                   , help="Specify aircraft model group, for example, '+35+BONANZA+SERIES'."
+                   , type=str)
+parser.add_argument( "-mh"
+                   , "--min_hours"
+                   , default=125
+                   , help="Enter the minimum hours you want to fly per year as an integer."
+                   , type=int)
+parser.add_argument( "-mt"
+                   , "--model_type"
+                   , default="V35"
+                   , help="Specify a model subtype, all capital string, for example V35 or V35A."
+                   , type=str) 
+parser.add_argument( "-o"
+                   , "--output_file"
+                   , default="out.csv"
+                   , help="Specify outoutfile."
+                   , type=str)
+parser.add_argument( "-occ"
+                   , "--oil_change_cost"
+                   , default=125
+                   , help="Enter the oil change cost per 50 hours as an integer, no symbols or commas."
+                   , type=int)
+parser.add_argument( "-och"
+                   , "--oil_cost_per_hour"
+                   , default=2.0
+                   , help="Enter the oil cost per hour as a float, no symbols or commas."
+                   , type=float)
+parser.add_argument( "-poc"
+                   , "--prop_overhaul_cost"
+                   , default=8000
+                   , help="Enter the cost of a prop overhaul as an integer, no symbols or commas."
+                   , type=int)
+parser.add_argument( "-ptbo"
+                   , "--prop_time_between_overhauls"
+                   , default=2000
+                   , help="Enter prop time between overhauls as an integer, no symbols or commas."
+                   , type=int)
+parser.add_argument( "-ts"
+                   , "--total_to_spend"
+                   , default=25000
+                   , help="Enter the total amount to spend per year in dollars without symbols or commas."
+                   , type=int)
 
 args=parser.parse_args()
 
 # Constants
-YEARLY_TOTAL_TO_SPEND = args.total_to_spend  # $
-MIN_HOURS = args.min_hours  # hours
-
-YEARLY_INSURANCE = args.insurance  # $
-YEARLY_MAINENANCE = args.maintenance  # $
-YEARLY_HANGAR = args.hangar * 12 # $
-LOAN_PERC = args.loan_percent  # %
-LOAN_LENGTH = args.loan_length  # years
-
+DOWN_PAYMENT_PERCENT = args.down_payment_percent
 ENGINE_OVERHAUL_COST = args.engine_overhaul_cost  # $
 ENGINE_OVERHAUL_TIME = args.engine_time_between_overhauls  # hours
-
+GAS_PER_GALLON = args.gas_cost  # $
+GALLONS_PER_HOUR = args.gallons_per_hour  # gallons
+LOAN_LENGTH = args.loan_length  # years
+LOAN_PERC = args.loan_percent  # %
+MAKE = args.make
+MIN_HOURS = args.min_hours  # hours
+MODEL_GROUP = args.model_group
+MODEL_TYPE = args.model_type
+OIL_CHANGE_COST_PER_50_HOURS = args.oil_change_cost  # $
+OIL_COST_PER_HOUR = args.oil_cost_per_hour  # $
+OUT_FILE = args.output_file
 PROP_OVERHAUL_COST = args.prop_overhaul_cost  # $
 PROP_OVERHAUL_TIME = args.prop_time_between_overhauls  # hours
-
-OIL_CHANGE_COST_PER_50_HOURS = args.oil_change_cost  # $
-
-GAS_PER_GALLON = args.gas_cost  # $
-GAS_PER_HOUR = args.gallons_per_hour  # gallons
-
-OIL_COST_PER_HOUR = args.oil_cost_per_hour  # $
-
-OUT_FILE = args.output_file
-
-MAKE = args.make
-
-MODEL_GROUP = args.model_group
-
-MODEL_TYPE = args.model_type
-
-DOWN_PAYMENT_PERCENT = args.down_payment_percent
-
-
+YEARLY_HANGAR = args.hangar * 12 # $
+YEARLY_INSURANCE = args.insurance  # $
+YEARLY_MAINENANCE = args.maintenance  # $
+YEARLY_TOTAL_TO_SPEND = args.total_to_spend  # $
 
 TIME_BETWEEN_REQUESTS = 90  # seconds
 
@@ -113,15 +180,15 @@ for i in range(0, len(v35_list), 3):
         prop_time = v35_soup.find("label", text='Prop 1 Time:')
         if total_time and engine_time and prop_time:
             total_time = total_time.next_sibling.strip()
-            engine_time = int(re.findall("\d+", engine_time.next_sibling.strip())[0])
-            prop_time = int(re.findall("\d+", prop_time.next_sibling.strip())[0])
+            engine_time = int(re.findall(r"\d+", engine_time.next_sibling.strip())[0])
+            prop_time = int(re.findall(r"\d+", prop_time.next_sibling.strip())[0])
     
             loan_after_downpayment = price * (1.0-DOWN_PAYMENT_PERCENT)
             loan = -np.pmt(LOAN_PERC/12, LOAN_LENGTH*12, loan_after_downpayment)
 
             total_fixed = loan + YEARLY_HANGAR + YEARLY_INSURANCE + YEARLY_MAINENANCE
 
-            variable_hourly = (  GAS_PER_GALLON * GAS_PER_HOUR
+            variable_hourly = (  GAS_PER_GALLON * GALLONS_PER_HOUR
                                + OIL_COST_PER_HOUR
                                + OIL_CHANGE_COST_PER_50_HOURS/50.0
                                + ENGINE_OVERHAUL_COST/(ENGINE_OVERHAUL_TIME - engine_time)
