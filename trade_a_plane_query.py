160511b52e59c56340cf3f653f00295a7c079de9
Mark Agate <magate@email.arizona.edu>
2020-08-23 14:23:22 -0700

    Comment code.

--- trade_a_plane_query.py
+++ trade_a_plane_query.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+'''Create an HTML parser to query trade-a-plane based on how much money to spend per year
+   and how many hours to fly per year.'''
 import argparse
 from bs4 import BeautifulSoup
 import csv
-import numpy as np
+import numpy as np  # TODO: Update this to numpy-financial instead.
 import re
 import requests
 import sys
@@ -56,7 +58,6 @@ parser.add_argument( "-lp"
                    , type=float)
 parser.add_argument( "-m"
                    , "--make"
-                   , default='BEECHCRAFT'
                    , help="Specify aircraft make as all capital string."
                    , type=str)
 parser.add_argument( "-maint"
@@ -66,7 +67,6 @@ parser.add_argument( "-maint"
                    , type=int)
 parser.add_argument( "-mg"
                    , "--model_group"
-                   , default="+35+BONANZA+SERIES"
                    , help="Specify aircraft model group, for example, '+35+BONANZA+SERIES'."
                    , type=str)
 parser.add_argument( "-mh"
@@ -76,7 +76,6 @@ parser.add_argument( "-mh"
                    , type=int)
 parser.add_argument( "-mt"
                    , "--model_type"
-                   , default="V35"
                    , help="Specify a model subtype, all capital string, for example V35 or V35A."
                    , type=str) 
 parser.add_argument( "-o"
@@ -113,20 +112,21 @@ parser.add_argument( "-ts"
 args=parser.parse_args()
 
 # Constants
-DOWN_PAYMENT_PERCENT = args.down_payment_percent
+DOWN_PAYMENT_PERCENT = args.down_payment_percent  # %
 ENGINE_OVERHAUL_COST = args.engine_overhaul_cost  # $
 ENGINE_OVERHAUL_TIME = args.engine_time_between_overhauls  # hours
 GAS_PER_GALLON = args.gas_cost  # $
 GALLONS_PER_HOUR = args.gallons_per_hour  # gallons
 LOAN_LENGTH = args.loan_length  # years
 LOAN_PERC = args.loan_percent  # %
-MAKE = args.make
+MAKE = args.make  # str
 MIN_HOURS = args.min_hours  # hours
-MODEL_GROUP = args.model_group
-MODEL_TYPE = args.model_type
+MODEL_GROUP = args.model_group  # str
+MODEL_TYPE = args.model_type  # str
+NUM_LINKS = 3  # Each table result has 3 links per aircraft. Remove duplication. TODO use unique.
 OIL_CHANGE_COST_PER_50_HOURS = args.oil_change_cost  # $
 OIL_COST_PER_HOUR = args.oil_cost_per_hour  # $
-OUT_FILE = args.output_file
+OUT_FILE = args.output_file  # str
 PROP_OVERHAUL_COST = args.prop_overhaul_cost  # $
 PROP_OVERHAUL_TIME = args.prop_time_between_overhauls  # hours
 YEARLY_HANGAR = args.hangar * 12 # $
@@ -134,74 +134,100 @@ YEARLY_INSURANCE = args.insurance  # $
 YEARLY_MAINENANCE = args.maintenance  # $
 YEARLY_TOTAL_TO_SPEND = args.total_to_spend  # $
 
+# This is needed to avoid the 429 from trade-a-plane.
+# I was not able to find what the time-out is but 90 seconds works well.
 TIME_BETWEEN_REQUESTS = 90  # seconds
 
-
+# Trade-a-plane single line advanced search.
 BASE_URL = 'https://www.trade-a-plane.com/'
 SEARCH_URL = ( BASE_URL 
-             + f'search?s-type=aircraft&s-advanced=yes&sale_status=For+Sale&category_level1=Single+Engine+Piston&make={MAKE}&model_group=BEECHCRAFT{MODEL_GROUP}&user_distance=1000000&s-custom_style=oneline&s-page_size=96'
+    + f'search?s-type=aircraft&s-advanced=yes&sale_status=For+Sale&category_level1=Single+Engine+Piston&make={MAKE}&model_group=BEECHCRAFT{MODEL_GROUP}&user_distance=1000000&s-custom_style=oneline&s-page_size=96')
 page = requests.get(SEARCH_URL)
 
+# Look for links in the results.
 soup = BeautifulSoup(page.content, 'html.parser')
 search_results = soup.find_all('a', class_="log_listing_click")
 
+# Find specified model in the results.
 aircraft_list = [result for result in search_results if MODEL_TYPE in str(result)]
 
 url_to_checkout = []
 hours_per_year = []
 
+# Print how long it will take with the 90 second wait.
 print(f'Approximate run time is {int(len(aircraft_list)/3*90/60)} minutes.\n')
 
-for i in range(0, len(aircraft_list), 3):
+# Loop through results.
+for i in range(0, len(aircraft_list), NUM_LINKS):
     aircraft = aircraft_list[i]
+    # Parse aircraft string for url.
     s = str(aircraft)
     start = s.find('href="') + len('href=""')
     end = s.find('type=aircraft"') + len('type=aircraft')
     aircraft_url = s[start:end]
     
+    # Don't send too many requests.
     time.sleep(TIME_BETWEEN_REQUESTS)
+
+    # Find the aircraft page.
     aircraft_page = requests.get(BASE_URL + aircraft_url)
     aircraft_soup = BeautifulSoup(aircraft_page.content, 'html.parser')
     
+    # TODO: Throw an error if too many requests. This can be handled more gracefully.
     while '429 Too Many Requests' in str(aircraft_soup):
         print(aircraft_soup)
         sys.exit(0)
 
-    print(f'Request {i/3+1} of {int(len(aircraft_list)/3)} successful.')
+    # Keep the user updated on progress.
+    print(f'Request {i/NUM_LINKS+1} of {int(len(aircraft_list)/NUM_LINKS)} successful.')
     
+    # TODO: Parse GPS string as input.
+    # Only considering Garmin GPS.
     if 'GARMIN' or 'GNS' or 'G430' or 'G530' in str(aircraft_soup):
         aircraft_soup.find('p', class_="price")
         s = str(aircraft_soup)
         start = s.find('> $') + len('> $')
         end = s.find(' <span itemprop="priceCurrency"')
         price = int(s[start:end].replace(',',''))
-    
+        
+        # Parse information.
         total_time = aircraft_soup.find("label", text='Total Time:')
         engine_time = aircraft_soup.find("label", text='Engine 1 Time:')
         prop_time = aircraft_soup.find("label", text='Prop 1 Time:')
         if total_time and engine_time and prop_time:
+            # Strip into a string.
             total_time = total_time.next_sibling.strip()
             engine_time = int(re.findall(r"\d+", engine_time.next_sibling.strip())[0])
             prop_time = int(re.findall(r"\d+", prop_time.next_sibling.strip())[0])
-    
+            
+            # Calculate loan based on listed price and down payment percentage.
             loan_after_downpayment = price * (1.0-DOWN_PAYMENT_PERCENT)
+
+            # Calculate loan payments.
             loan = -np.pmt(LOAN_PERC/12, LOAN_LENGTH*12, loan_after_downpayment)
-
+            
+            # Total fixed costs yearly.
             total_fixed = loan + YEARLY_HANGAR + YEARLY_INSURANCE + YEARLY_MAINENANCE
-
+            
+            # Total variable costs per hour.
             variable_hourly = (  GAS_PER_GALLON * GALLONS_PER_HOUR
                                + OIL_COST_PER_HOUR
                                + OIL_CHANGE_COST_PER_50_HOURS/50.0
                                + ENGINE_OVERHAUL_COST/(ENGINE_OVERHAUL_TIME - engine_time)
                                + PROP_OVERHAUL_COST/(PROP_OVERHAUL_TIME - prop_time)
                               )
-    
+            
+            # Based on amount willing to spend per year, how many hours are remaining to fly.
             yearly_hours = int((YEARLY_TOTAL_TO_SPEND - total_fixed)/variable_hourly)
-    
+            
+            # Make sure hours remaining to fly are meaningful.
             if yearly_hours > MIN_HOURS:
                 url_to_checkout.append(aircraft_url)
                 hours_per_year.append(yearly_hours)
 
+# Print to terminal and create a csv with results.
+# Order by most hours able to fly.
+# List hours able to fly per year and link to the aircraft.
 if hours_per_year and url_to_checkout:
     hours_per_year, url_to_checkout = (list(t) for t in zip(*sorted(zip(hours_per_year, url_to_checkout), reverse=True)))
                                    
@@ -211,5 +237,6 @@ if hours_per_year and url_to_checkout:
             print(str(hours_per_year[i]) + ',' + str(BASE_URL) + str(url_to_checkout[i]))
             print('\n')
             writer.writerow([str(hours_per_year[i]), str(BASE_URL) + str(url_to_checkout[i])])   
+# Let the user know if there are no results for the given criteria.
 else:
     print(f'No results for ${YEARLY_TOTAL_TO_SPEND} yearly and minimum {MIN_HOURS} hours per year.')
